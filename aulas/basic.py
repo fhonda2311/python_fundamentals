b387ebc79c08e6ab93e32bfd6454d6d382410f37
Fabio Honda <fabiohonda@mac.com>
2020-03-04 22:26:01 -0300

    aula 01

---
+++ aulas/basic.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+
+# print('oi meu nome eh' ,nome) # primeira opcao
+# print('oi meu nome eh {}' .format(nome)) # segunda opcao
+# print(f'oi meu nome eh {nome}')# terceira opcao
+
+# tipos de dados
+
+# print(type(nome))
+# print(type(idade))
+# print(type(altura))
+# print(type(brasileiro))
+
+#entrada de dados
+#nome = input('qual eh seu nome? ')
+
+#print(f'o nome eh {nome}')
+
+#num1 = int(input('Digite o primeiro numero: '))
+#num2 = int(input('Digite o segundo numero: '))
+
+#print(num1+num2)
+
+
+texto = 'super texto  gigante acba aqui.\
+        so que nao.\
+        mas poderia sim.'
+
+print(texto.replace('nao'))  
---
+++ exercicios/basic_ex.py
@@ -0,0 +1,9 @@
+# Crie um script com duas variaveis
+# Nome e sobrenome
+# e imprima na tela 
+# a mensagem 'ola meu nome e nome sobrenome'
+
+nome = 'fabio' 
+sobrenome = 'honda'
+print (f'ola meu nome eh {nome} {sobrenome}')
+
